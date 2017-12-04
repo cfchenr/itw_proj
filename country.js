910051c8f7e6161de2c39afd029fa1643f14a804
Fábio Henriques <fabiomchenriques@ua.pt>
2017-12-04 18:17:13 +0000

    Add new files

---
+++ country.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+	var vm = function() {
+		console.log('ViewModel initiated...');
+		//---Variáveis locais
+		var self = this;
+		var url = window.location.href;
+		var countryID = url.split("=")[1];
+		var baseUri = 'http://192.168.160.28/football/api/countries/'+countryID;
+		self.className = 'Countries';
+		self.description = '';
+		self.error = ko.observable();
+		self.country = ko.observableArray([]);
+		self.league = ko.observableArray([]);
+		//--- Internal functions
+		function ajaxHelper(uri, method, data) {
+			self.error(''); //Clear error message
+			return $.ajax({
+				type: method,
+				url: uri,
+				dataType: 'json',
+				contentType: 'application/json',
+				data: data ? JSON.stringify(data) : null,
+				error: function(jqXHR, textStatus, errorThrown) {
+					console.log("AJAX Call [" + uri + "] Fail...");
+					self.error(errorThrown);
+				}
+			})
+		}
+		//--- External functions (accessible outside)
+		self.getCountry = function() {
+			console.log('CALL: getCountry...');
+			ajaxHelper(baseUri, 'GET').done(function(data) {
+				self.country(data);
+				baseUri = 'http://192.168.160.28/football/api/leagues/'+leagueID;
+				ajaxHelper(baseUri, 'GET').done(function(data) {
+					self.league(data);
+				});
+			});
+		};
+		//--- Initial call
+		self.getCountry();
+	};
+	ko.applyBindings(vm);
+});
--- main.js
+++ main.js
@@ -1,37 +1,3 @@
 $(document).ready(function () {
-	var vm = function() {
-		console.log('ViewModel initiated...');
-		//---Variáveis locais
-		var self = this;
-		var baseUri = 'http://192.168.160.28/football/api/countries';
-		self.className = 'Countries';
-		self.description = '';
-		self.error = ko.observable();
-		self.countries = ko.observableArray([]);
-		//--- Internal functions
-		function ajaxHelper(uri, method, data) {
-			self.error(''); //Clear error message
-			return $.ajax({
-				type: method,
-				url: uri,
-				dataType: 'json',
-				contentType: 'application/json',
-				data: data ? JSON.stringify(data) : null,
-				error: function(jqXHR, textStatus, errorThrown) {
-					console.log("AJAX Call [" + uri + "] Fail...");
-					self.error(errorThrown);
-				}
-			})
-		}
-		//--- External functions (accessible outside)
-		self.getCountries = function() {
-			console.log('CALL: getCountries...');
-			ajaxHelper(baseUri, 'GET').done(function(data) {
-				self.countries(data);
-			});
-		};
-		//--- Initial call
-		self.getCountries();
-	};
-	ko.applyBindings(vm);
+	$("#navbar").load('navbar.html');
 });
